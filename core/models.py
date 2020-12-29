b8b6cc3f08860ba2527e9499c7ebdeb3605444e3
Julio <jn5.guape@gmail.com>
2020-12-29 20:15:44 -0300

    models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from hashlib import md5
+from django.utils import timezone
 
-# Create your models here.
+
+class Url(models.Model):
+    full_url = models.URLField(unique=True)
+    short_url = models.URLField(unique=True)
+    clicks = models.IntegerField(default=0)
+    created = models.DateTimeField(default=timezone.now, blank=True)
+
+    def clicked(self):
+        self.clicks += 1
+        self.save()
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.url_hash = md5(self.full_url.encode()).hexdigest()[:10]
+
+        return super().save(*args, **kwargs)
--- encurtador/settings.py
+++ encurtador/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'shorty.schema.schema',
+}
